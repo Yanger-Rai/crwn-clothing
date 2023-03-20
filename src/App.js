003efd215e2503e9d3f54c49790a43cbbcec1bca
Yanger Rai <v.bikashrai@gmail.com>
2023-03-20 14:34:29 +0530

    Building an e-commerce website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+// import CategoryItem from "./components/category-item/category-item.component";
+import Directory from "./components/directory/directory.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Directory />;
+};
 
 export default App;
